5eded7f0bcaef5fd78466293ff4b7f107b22b8ee
Sarvarbek <ismatovsarvar76@gmail.com>
2025-04-17 13:38:23 +0500

    s

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const addSection = document.querySelector(".contact-form");
 const container = document.querySelector(".container");
 const usersWrapper = document.querySelector(".users-wrapper");
 const base_url = "https://67ff733858f18d7209f13d82.mockapi.io/users_num";
+const change = document.querySelector(".change");
+const del = document.querySelector(".del");
 add.addEventListener("click", () => {
   addSection.classList.toggle("open");
 });
@@ -30,6 +32,7 @@ async function postData(data) {
 
 const addBtn = document.querySelector(".add-btn");
 addBtn.addEventListener("click", () => {
+  addSection.classList.remove("open");
   const newUser = {
     phone: document.querySelector(".phone").value,
     name: document.querySelector(".name").value,
@@ -62,6 +65,47 @@ function userCard(data) {
         <p class="number-text">
            ${data.phone} 
         </p>
+         <img class="del"  data-id="${data.id}" src="./627249-delete3-1024.webp" alt="" />
+        <img class="change" src="pen.png" alt="">
   `;
   usersWrapper.appendChild(div);
 }
+
+async function updateUser(id, updatedData) {
+  try {
+    const response = await fetch(`${base_url}/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedData),
+    });
+
+    const data = await response.json();
+    console.log("Updated:", data);
+    fetchData();
+  } catch (error) {
+    console.error("Xatolik:", error);
+  }
+}
+
+usersWrapper.addEventListener("click", (e) => {
+  if (e.target.classList.contains("del")) {
+    const userId = e.target.dataset.id;
+    deleteUser(userId);
+  }
+});
+async function deleteUser(id) {
+  try {
+    const response = await fetch(`${base_url}/${id}`, {
+      method: "DELETE",
+    });
+    fetchData();
+    const data = await response.json();
+    console.log("Updated:", data);
+    fetchData();
+  } catch (error) {
+    console.error("Xatolik:", error);
+  }
+}
+console.log(true);
